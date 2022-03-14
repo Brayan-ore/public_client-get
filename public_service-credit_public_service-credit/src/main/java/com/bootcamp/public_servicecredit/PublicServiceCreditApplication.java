3681932eefbac0f791c1e3b161dc1da05d571164
jlamaspi <jlamaspi@LIM-8JH59K3.usersad.everis.int>
2022-03-14 10:47:16 -0500

    Feat: 'initialize application'

---
+++ public_service-credit_public_service-credit/src/main/java/com/bootcamp/public_servicecredit/PublicServiceCreditApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.public_servicecredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicServiceCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicServiceCreditApplication.class, args);
+	}
+
+}
